1b660174c7259bcb86552cd319ae2dd4e290e764
jmdc <johnmdcampos@gmail.com>
2020-04-23 08:22:38 -0300

    mudança na varável de ambiente

--- src/services/Config.js
+++ src/services/Config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://bd_user:bduser@cluster0-02jjc.mongodb.net/task?retryWrites=true&w=majority', {useNewUrlParser : true});
+mongoose.connect(MONGODB_URL, {useNewUrlParser : true});
